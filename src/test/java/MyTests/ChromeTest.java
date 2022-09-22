dec0d213266d601f2dc90521be5008e3eaa6a7fd
sagar_mopagar <sagarmopagar@gmail.com>
2022-09-22 21:08:12 +0530

    Version 1.0 - Simple Chrome Test

---
+++ src/test/java/MyTests/ChromeTest.java
@@ -0,0 +1,41 @@
+package MyTests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import static org.junit.Assert.assertThat;
+
+public class ChromeTest {
+
+    WebDriver driver;
+
+    @BeforeClass
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void test() {
+        // Exercise
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        String title = driver.getTitle();
+
+        // Verify
+        Assert.assertEquals(title,"Selenium WebDriver");
+    }
+
+
+}
